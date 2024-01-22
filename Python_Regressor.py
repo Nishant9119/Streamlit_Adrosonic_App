cce1de32a10d3f059bef197f17048f27c5fe1de5
Nishant Sharma <75371277+Nishant9119@users.noreply.github.com>
2024-01-22 12:03:49 +0000

    git commit "editing"

--- Python_Regressor.py
+++ Python_Regressor.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score,precision_score, recall_score, f1_score, confusion_matrix
 import pickle
-df = pd.read_csv("C:/Users/Nikhil Sharma/Downloads/final_smoted_claims_data.csv")
+df = pd.read_csv("/workspaces/Streamlit_Adrosonic_App/data_file/final_smoted_claims_data.csv")
 x = df.drop(["FraudFound_Yes",'X'],axis= 1)
 y = df['FraudFound_Yes']
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.25)
